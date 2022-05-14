07a0bd0173eb91f44fa38577a7cd48e791da9568
Daniel <daniel.munoz.melendez@unillanos.edu.co>
2022-05-14 12:22:21 -0500

    Actualización en el logout tomando el token de la sesion

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('index.php', function () {
+    return view('welcome');
+});
 Route::get('login.php', function () {
     return view('login');
 });
